b9451d4e1dbfd81fde184d0a0ef56d3e2fecc454
Jarle Kramer <jarlekramer@gmail.com>
2024-12-31 13:26:43 +0100

    update

--- visualization/stormbird_dashboard_frontend/src/app/components/ForcePlot.tsx
+++ visualization/stormbird_dashboard_frontend/src/app/components/ForcePlot.tsx
@@ -1,70 +1,56 @@
 'use client';
 
-import React, { useEffect } from 'react';
-import dynamic from 'next/dynamic';
-
-// Dynamically import Plotly without SSR
-const Plotly = dynamic(() => import('plotly.js-dist-min'), { ssr: false });
+import React, { useEffect, useState } from 'react';
 
 const ForcePlot = () => {
+    const [data, setData] = useState(
+        [
+            { x: [], y: [], type: 'scatter', mode: 'lines', name: 'Force in x direction' }, 
+            { x: [], y: [], type: 'scatter', mode: 'lines', name: 'Force in y direction' }
+        ]
+    );
+    
+    const [layout, setLayout] = useState(
+        {
+            title: 'Integrated forces as a function of time', 
+            xaxis: { title: 'Time' }, 
+            yaxis: { title: 'Value' }
+        }
+    );
+    
+    const [Plot, setPlot] = useState(null);
+
     useEffect(() => {
-        const loadPlotly = async () => {
-            const Plotly = await import('plotly.js-dist-min');
+        import('react-plotly.js').then((module) => {
+            setPlot(() => module.default);
+        });
 
-            const x_trace = {
-                x: [],
-                y: [],
-                type: 'scatter',
-                mode: 'lines',
-                name: 'Force in x direction',
-            };
-            
-            const y_trace = {
-                x: [],
-                y: [],
-                type: 'scatter',
-                mode: 'lines',
-                name: 'Force in y direction',
-            };
-            
-            const layout = {
-                title: 'Integrated forces as a function of time',
-                xaxis: { title: 'Time' },
-                yaxis: { title: 'Value' }
-            };
-            
-            const fetchInterval = 500;
-            
-            const divName = 'forcePlot';
-            
-            Plotly.newPlot(divName, [x_trace, y_trace], layout);
-            
-            const intervalId = setInterval(() => {
-                fetch('http://localhost:8080/get-forces')
-                    .then(response => response.text())
-                    .then(text => {
-                        if (text) {
-                            const data = JSON.parse(text);
-                
-                            x_trace.x = data.time;
-                            x_trace.y = data.force_x;
-                
-                            y_trace.x = data.time;
-                            y_trace.y = data.force_y;
-                            
-                            Plotly.redraw(divName);
-                        }
-                    })
-                    .catch(error => console.error('Error fetching data:', error));
-            }, fetchInterval);
+        const fetchInterval = 500;
 
-            return () => clearInterval(intervalId);
-        };
+        const intervalId = setInterval(() => {
+            fetch('http://localhost:8080/get-forces')
+                .then(response => response.text())
+                .then(text => {
+                    if (text) {
+                        const fetchedData = JSON.parse(text);
 
-        loadPlotly();
+                        setData([
+                            { x: fetchedData.time, y: fetchedData.force_x, type: 'scatter', mode: 'lines', name: 'Force in x direction' },
+                            { x: fetchedData.time, y: fetchedData.force_y, type: 'scatter', mode: 'lines', name: 'Force in y direction' }
+                        ]);
+                    }
+                })
+                .catch(error => console.error('Error fetching data:', error));
+        }, fetchInterval);
+
+        return () => clearInterval(intervalId);
     }, []);
 
-    return <div id="forcePlot" />;
+    if (!Plot) {
+        return <div>Loading...</div>;
+    }
+
+    return <Plot data={data} layout={layout} />;
 };
 
 export default ForcePlot;
