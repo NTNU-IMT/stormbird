c98195253a8f05be235bf2b486df141f8e2e9c20
Jarle Kramer <jarlekramer@gmail.com>
2026-09-09 20:31:33 +0200

    Switch back the InletOutlet boundary conditions

--- stormflow/src/velocity_solver/boundary_condisitions.rs
+++ stormflow/src/velocity_solver/boundary_condisitions.rs
@@ -135,19 +135,20 @@ impl VelocityBoundaryConditions {
                         // which flow direction counts as inflow, without needing `face_index` here.
                         let at_the_min_boundary_face = boundary_face.neighbor_delta > 0;
 
-                        //let neighbor_axis_flow = velocity[flat_neighbor][axis_index];
+                        let neighbor_axis_flow = velocity[flat_neighbor][axis_index];
 
-                        let extended_indices = grid.extended_indices_from_flat_index(flat_current);
-                        let cell_center = grid.cell_center_extended(extended_indices);
-                        let domain_flow = self.velocity_at_point(cell_center)[axis_index];
+                        //let domain_flow = self.velocity_at_point(cell_center)[axis_index];
 
                         let inflow = if at_the_min_boundary_face{
-                            domain_flow > 0.0
+                            neighbor_axis_flow > 0.0
                         } else {
-                            domain_flow < 0.0
+                            neighbor_axis_flow < 0.0
                         };
 
                         if inflow {
+                            let extended_indices = grid.extended_indices_from_flat_index(flat_current);
+                            let cell_center = grid.cell_center_extended(extended_indices);
+                            
                             let mut new_value = SpatialVector::default();
                             for c in 0..3 {
                                 let mut face_point = cell_center;
